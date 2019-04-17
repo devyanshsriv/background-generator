53b4dae78649e07b337e5311874e0f7e575ae24d
devyanshsriv <dev.sri1913@gmail.com>
2019-04-17 18:01:27 +0530

    uploading background generator

---
+++ Background Generator/script1.js
@@ -0,0 +1,16 @@
+const basket = ['apples', 'oranges', 'grapes'];
+
+for (let i = 0; i<basket.length; i++) {
+	console.log(basket[i]);
+}
+
+
+basket.forEach(item =>{
+	console.log(item);
+})
+
+
+ 
+for (item of basket) {
+	console.log(item);
+}
